26a15c8ddac6aef3b3162ea3e293a2e879746257
gustadev24 <lgsc21211@gmail.com>
2025-06-12 14:12:43 -0500

    refactor: reorganize files and directories

--- src/modules/core/ui/link.tsx
+++ src/modules/core/ui/link.tsx
@@ -1,4 +1,4 @@
-import { cn } from '@/lib/utils';
+import { cn } from '@/modules/core/lib/utils';
 import { Button, buttonVariants } from './button';
 import { type VariantProps } from 'class-variance-authority';
 import React from 'react';
