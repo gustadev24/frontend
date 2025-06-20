6d4c31f6c0df4464f4d45d22d8c4b30fbce5fcc7
gustadev24 <lgsc21211@gmail.com>
2025-06-20 11:22:44 -0500

    feat(forms): add infer field component and use on login form

---
+++ src/modules/core/types/field.ts
@@ -0,0 +1,33 @@
+import { SupportedFields } from '@/modules/core/lib/field';
+import type { SelectOption } from '@/modules/core/types/select';
+
+export type SupportFieldType =
+  (typeof SupportedFields)[keyof typeof SupportedFields];
+
+type BaseField<T extends string> = {
+  name: T;
+  label: string;
+  description?: string;
+  placeholder?: string;
+};
+
+export type Field<T extends string> = BaseField<T> &
+  (
+    | {
+        type:
+          | typeof SupportedFields.EMAIL
+          | typeof SupportedFields.PASSWORD
+          | typeof SupportedFields.TEXTAREA
+          | typeof SupportedFields.TEXT
+          | typeof SupportedFields.NUMBER;
+      }
+    | {
+        type: typeof SupportedFields.SELECT;
+        options: SelectOption[];
+      }
+    | {
+        type: typeof SupportedFields.OTP;
+        maxLength: number;
+        pattern: string;
+      }
+  );
