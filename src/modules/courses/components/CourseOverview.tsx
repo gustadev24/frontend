c4062ed8d20dd503b2f41b05d6c84188511960ca
gustadev24 <lgsc21211@gmail.com>
2025-06-21 12:43:31 -0500

    refactor: rename types and fix type errors with courses

--- src/modules/courses/components/CourseOverview.tsx
+++ src/modules/courses/components/CourseOverview.tsx
@@ -1,4 +1,4 @@
-import { useCourses } from '../hooks/useCourses';
+import { useCourses } from '@/modules/courses/lib/useCourses';
 
 function TagSkeleton() {
   return <div className="w-24 h-6 bg-gray-200 animate-pulse rounded-full" />;
