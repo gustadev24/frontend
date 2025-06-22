59f9cb1712ad222c3e28dc5d39c3abd526f6ef9c
gustadev24 <lgsc21211@gmail.com>
2025-06-22 16:36:36 -0500

    chore(post): set same preview an view of a post

--- src/modules/post/lib/usePosts.ts
+++ src/modules/post/lib/usePosts.ts
@@ -115,6 +115,10 @@ Puedes aplicar estilos fácilmente:
 > Este es un ejemplo de cita. Puedes usarlo para destacar ideas o fragmentos de otros autores.
 
 ### Bloques de código
+\`\`\`javascript
+const greeting = 'Hello, Javascript!';
+console.log(greeting);
+\`\`\`
 `,
     module: {
       title: 'TypeScript Basics',
